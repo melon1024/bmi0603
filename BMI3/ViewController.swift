f9be2d9220b8f609d8a7dabe053773eafb205f7a
Myoung-Wan Koo <mwkoo9@gmail.com>
2016-03-30 13:53:11 +0900

    Initial Commit

---
+++ BMI3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMI3
+//
+//  Created by Myoung-Wan Koo on 2016. 3. 30..
+//  Copyright © 2016년 Myoung-Wan Koo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
